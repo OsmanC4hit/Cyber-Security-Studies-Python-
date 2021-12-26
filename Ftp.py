340bf5a7718cb9ba4de2d972d491eefb7afb4ac6
Osman Cahit Yüksel <71992282+OsmanC4hit@users.noreply.github.com>
2021-12-27 00:27:08 +0300

    Add files via upload

---
+++ Ftp.py
@@ -0,0 +1,12 @@
+import ftplib
+host = input("İp adres giriniz: ")
+def Login(HostName):
+    try:
+        ftp = ftplib.FTP(HostName)
+        ftp.login("anonymous","anonymous")
+        print("[+] " + HostName + " Giriş başarılı")
+        ftp.quit
+        return True
+    except Exception as e:
+        print("[-] " + HostName + " Giriş başarısız")
+Login(host)
